5cdaaa956bb7b37c33b7f26ebcb750ae463da2be
Liyuan Cao <hillcao12002@gmail.com>
2022-11-21 17:16:48 +0800

    Fixed terrain/Compile terrain related classes

---
+++ src/main/bq3/terrain/Desert.java
@@ -0,0 +1,12 @@
+package bq3.terrain;
+
+/**
+ * Desert
+ *
+ * @author Liyuan Cao
+ */
+public class Desert extends Terrain {
+    public Desert(String name, String desc) {
+        super(name, desc);
+    }
+}
---
+++ src/main/bq3/terrain/Terrain.java
@@ -0,0 +1,42 @@
+package bq3.terrain;
+
+/**
+ * Terrain
+ *
+ * @author Liyuan Cao
+ */
+public class Terrain {
+
+    /**
+     * name
+     */
+    private String name;
+
+    /**
+     * describe
+     */
+    private String desc;
+
+    public Terrain(){}
+
+    public Terrain(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
