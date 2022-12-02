bd0c6cfdc913b353258347beadc8e5f4bd382da7
031205 <bolin.shen21@ycdsbk12.ca>
2022-12-02 03:54:24 -0500

    end

--- src/main/bq3/end.java
+++ src/main/bq3/end.java
@@ -11,4 +11,30 @@ public class Settlement {
     private Player getWinner(Player player1, Player player2){
         return player1.getCost()>player2.getCost()?player1:player2;
     }
+
+    private void displayArms(Player player){
+        Armnew[] armlist = player.getArmlist();
+        System.out.println("Here is your arms");
+        for (Armnew armnew : armlist) {
+            System.out.println(armnew);
+            System.out.println(armnew.getHp());
+            System.out.println(armnew.getMovementTotal());
+        }
+    }
+
+    private void displayNation(Player player){
+        System.out.println(player.getNation());
+    }
+
+    private void display(Player player1, Player player2){
+        System.out.println("Here is the players' information:");
+        displayArms(player1);
+        displayNation(player1);
+        System.out.println("=======================");
+        displayNation(player2);
+        displayArms(player2);
+        System.out.println("=======================");
+        System.out.println("The winner is:");
+        System.out.println(getWinner(player1, player2));
+    }
 }
